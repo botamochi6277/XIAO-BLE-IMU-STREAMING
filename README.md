5947adf4f5532ba6bf18f159afe1eb9d3df68cfe
botamochi6277 <botamochi6277@icloud.com>
2022-06-28 23:28:20 +0900

    initial acc commit

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 - acc access
 - streaming data with serial port
 - streaming data with BLE
+
+## References
+
+- https://wiki.seeedstudio.com/XIAO-BLE-Sense-IMU-Usage/
+
+- [LSM6DS3.h](https://raw.githubusercontent.com/Seeed-Studio/Seeed_Arduino_LSM6DS3/master/LSM6DS3.h)
+- [LSM6DS3.cpp](https://raw.githubusercontent.com/Seeed-Studio/Seeed_Arduino_LSM6DS3/master/LSM6DS3.cpp)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,55 @@
-#include <Arduino.h>
+// based on https://github.com/Seeed-Studio/Seeed_Arduino_LSM6DS3/blob/master/examples/HighLevelExample/HighLevelExample.ino
 
-void setup() {
+#include <Arduino.h>
+#include "LSM6DS3.h"
+#include "Wire.h"
+
+// Create a instance of class LSM6DS3
+LSM6DS3 myIMU(I2C_MODE, 0x6A); // I2C device address 0x6A
+
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  // Call .begin() to configure the IMUs
+  if (myIMU.begin() != 0)
+  {
+    Serial.println("Device error");
+  }
+  else
+  {
+    Serial.println("Device OK!");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  // Accelerometer
+  Serial.print("\nAccelerometer:\n");
+  Serial.print(" X1 = ");
+  Serial.println(myIMU.readFloatAccelX(), 4);
+  Serial.print(" Y1 = ");
+  Serial.println(myIMU.readFloatAccelY(), 4);
+  Serial.print(" Z1 = ");
+  Serial.println(myIMU.readFloatAccelZ(), 4);
+
+  // Gyroscope
+  Serial.print("\nGyroscope:\n");
+  Serial.print(" X1 = ");
+  Serial.println(myIMU.readFloatGyroX(), 4);
+  Serial.print(" Y1 = ");
+  Serial.println(myIMU.readFloatGyroY(), 4);
+  Serial.print(" Z1 = ");
+  Serial.println(myIMU.readFloatGyroZ(), 4);
+
+  // Thermometer
+  Serial.print("\nThermometer:\n");
+  Serial.print(" Degrees C1 = ");
+  Serial.println(myIMU.readTempC(), 4);
+  Serial.print(" Degrees F1 = ");
+  Serial.println(myIMU.readTempF(), 4);
+
+  delay(1000);
 }
