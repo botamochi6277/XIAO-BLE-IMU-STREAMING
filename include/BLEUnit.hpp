128137ba4e7d3036dd5a6da5b03b698976e08479
botamochi6277 <botamochi6277@icloud.com>
2023-04-14 19:47:28 +0900

    add BLE unit

---
+++ include/BLEUnit.hpp
@@ -0,0 +1,18 @@
+#ifndef BLE_UNIT_HPP
+#define BLE_UNIT_HPP
+
+#define BLE_GATT_CPF_UNIT_UNITLESS 0x2700
+#define BLE_GATT_CPF_UNIT_METER 0x2701   // length(meter)
+#define BLE_GATT_CPF_UNIT_KG 0x2702      // mass(kg)
+#define BLE_GATT_CPF_UNIT_SEC 0x2703     // time(second)
+#define BLE_GATT_CPF_UNIT_AMPERE 0x2704  // current intensity(ampere)
+#define BLE_GATT_CPF_UNIT_KELVIN 0x2705  // tmp(kelvin)
+
+#define BLE_GATT_CPF_UNIT_METER_PER_S 0x2712   // velocity(m/s)
+#define BLE_GATT_CPF_UNIT_METER_PER_SS 0x2713  // acc(m/s^2)
+
+#define BLE_GATT_CPF_UNIT_RAD 0x2720  // angle(radian)
+
+#define BLE_GATT_CPF_UNIT_RAD_PER_S 0x2743  // angular velocity (rad/s)
+
+#endif
