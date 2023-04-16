33bdfa9215625e376a398a0a964b891805bcb91e
botamochi6277 <botamochi6277@icloud.com>
2023-04-16 15:29:27 +0900

    test for  format descriptor

--- include/BLEFormat.hpp
+++ include/BLEFormat.hpp
@@ -1,4 +1,3 @@
-// https://btprodspecificationrefs.blob.core.windows.net/assigned-numbers/Assigned%20Number%20Types/Format%20Types.pdf
 // https://developer.nordicsemi.com/nRF5_SDK/nRF51_SDK_v4.x.x/doc/html/group___b_l_e___g_a_t_t___c_p_f___f_o_r_m_a_t_s.html
 #ifndef BLE_FORMAT_HPP
 #define BLE_FORMAT_HPP
@@ -26,8 +25,8 @@
 #define BLE_GATT_CPF_FORMAT_FLOAT32 0x14
 #define BLE_GATT_CPF_FORMAT_FLOAT64 0x15
 #define BLE_GATT_CPF_FORMAT_SFLOAT 0x16
-#define BLE_GATT_CPF_FORMAT_FLOAT 0x17
-#define BLE_GATT_CPF_FORMAT_DUINT16 0x18
+#define BLE_GATT_CPF_FORMAT_FLOAT 0x17    // medfloat
+#define BLE_GATT_CPF_FORMAT_DUINT16 0x18  // medfloat
 #define BLE_GATT_CPF_FORMAT_UTF8S 0x19
 #define BLE_GATT_CPF_FORMAT_UTF16S 0x1A
 #define BLE_GATT_CPF_FORMAT_STRUCT 0x1B
--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,84 @@
-// based on https://github.com/Seeed-Studio/Seeed_Arduino_LSM6DS3/blob/master/examples/HighLevelExample/HighLevelExample.ino
-// BLE GATT UUID: https://btprodspecificationrefs.blob.core.windows.net/assigned-values/16-bit%20UUID%20Numbers%20Document.pdf
+// based on
+// https://github.com/Seeed-Studio/Seeed_Arduino_LSM6DS3/blob/master/examples/HighLevelExample/HighLevelExample.ino
 #include <Arduino.h>
 #include <ArduinoBLE.h>
 
+#include "BLEFormat.hpp"
+#include "BLEUnit.hpp"
 #include "LSM6DS3.h"
 #include "Wire.h"
 
 // Create a instance of class LSM6DS3
-LSM6DS3 my_imu(I2C_MODE, 0x6A); // I2C device address 0x6A
+LSM6DS3 my_imu(I2C_MODE, 0x6A);  // I2C device address 0x6A
 
 // BLE Variables
 BLEService imu_service("ABF0E000-B597-4BE0-B869-6054B7ED0CE3");
 // clock Characteristic
-BLEUnsignedLongCharacteristic timer_characteristic("ABF0E001-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEUnsignedLongCharacteristic timer_characteristic(
+    "ABF0E001-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
 //  Characteristic User Description Descriptor
 BLEDescriptor timer_descriptor("2901", "timer_ms");
 unsigned long milli_sec = 0;
 
 // acc
-BLEFloatCharacteristic acc_x_characteristic("ABF0E002-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
-BLEFloatCharacteristic acc_y_characteristic("ABF0E003-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
-BLEFloatCharacteristic acc_z_characteristic("ABF0E004-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEFloatCharacteristic acc_x_characteristic(
+    "ABF0E002-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEFloatCharacteristic acc_y_characteristic(
+    "ABF0E003-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEFloatCharacteristic acc_z_characteristic(
+    "ABF0E004-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+// 0x2901 User Characteristic Description
 BLEDescriptor acc_x_descriptor("2901", "accX");
 BLEDescriptor acc_y_descriptor("2901", "accY");
 BLEDescriptor acc_z_descriptor("2901", "accZ");
 
+// 0x2904 Characteristic Presentation Format
+void presentation_format(uint8_t buff[], uint8_t format, uint8_t exp,
+                         u_int16_t unit) {
+  // format | exponent |unit|namespace|description
+
+  // u_int8_t buff[7];
+  buff[0] = format;
+  buff[1] = exp;  // value * power(10,exp)
+  buff[2] = unit;
+  buff[3] = unit >> 8;
+  buff[4] = 0x01;  // Bluetooth SIG
+  buff[5] = 0x00;  // description
+  buff[6] = 0x00;  // description
+  // BLEDescriptor descriptor("2904", buff, 7);
+  // return descriptor;
+  return;
+}
+uint8_t acc_format_array[7] = {
+    BLE_GATT_CPF_FORMAT_FLOAT32,
+    0x00,
+    (uint8_t)BLE_GATT_CPF_UNIT_METER_PER_SS,       // 0x13
+    (uint8_t)BLE_GATT_CPF_UNIT_METER_PER_SS >> 8,  // 0x27
+    0x01,
+    0x00,
+    0x00};  // byte array
+
+// https://github.com/espressif/arduino-esp32/blob/master/libraries/BLE/src/BLE2904.h
+BLEDescriptor test_descriptor("2905", "a");
+BLEDescriptor acc_format_descriptor("2904", acc_format_array, 7);
+
 // Gyro
-BLEFloatCharacteristic gyro_x_characteristic("ABF0E005-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
-BLEFloatCharacteristic gyro_y_characteristic("ABF0E006-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
-BLEFloatCharacteristic gyro_z_characteristic("ABF0E007-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEFloatCharacteristic gyro_x_characteristic(
+    "ABF0E005-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEFloatCharacteristic gyro_y_characteristic(
+    "ABF0E006-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEFloatCharacteristic gyro_z_characteristic(
+    "ABF0E007-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
 BLEDescriptor gyro_x_descriptor("2901", "gyroX");
 BLEDescriptor gyro_y_descriptor("2901", "gyroY");
 BLEDescriptor gyro_z_descriptor("2901", "gyroZ");
 
 // Temperature
-BLEFloatCharacteristic temp_characteristic("ABF0E008-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
+BLEFloatCharacteristic temp_characteristic(
+    "ABF0E008-B597-4BE0-B869-6054B7ED0CE3", BLERead | BLENotify);
 BLEDescriptor temp_descriptor("2901", "Temperature");
 
-void setup()
-{
+void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
@@ -52,24 +92,19 @@ void setup()
   digitalWrite(LEDB, HIGH);
 
   // Call .begin() to configure the IMUs
-  if (my_imu.begin() != 0)
-  {
+  if (my_imu.begin() != 0) {
     Serial.println("Device error");
     digitalWrite(LEDR, LOW);
-  }
-  else
-  {
+  } else {
     Serial.println("Device OK!");
   }
 
   // ble
   // begin initialization
-  if (!BLE.begin())
-  {
+  if (!BLE.begin()) {
     Serial.println("starting BLE failed!");
 
-    while (1)
-    {
+    while (1) {
       digitalWrite(LEDR, !digitalRead(LEDR));
       delay(500);
     };
@@ -91,6 +126,9 @@ void setup()
   acc_x_characteristic.addDescriptor(acc_x_descriptor);
   acc_y_characteristic.addDescriptor(acc_y_descriptor);
   acc_z_characteristic.addDescriptor(acc_z_descriptor);
+
+  acc_x_characteristic.addDescriptor(acc_format_descriptor);
+  // acc_x_characteristic.addDescriptor(test_descriptor);
 
   // gyro
   imu_service.addCharacteristic(gyro_x_characteristic);
@@ -119,14 +157,11 @@ void setup()
   BLE.advertise();
 }
 
-void loop()
-{
+void loop() {
   BLEDevice central = BLE.central();
 
-  if (central)
-  {
-    while (central.connected())
-    {
+  if (central) {
+    while (central.connected()) {
       milli_sec = millis();
       timer_characteristic.writeValueLE(milli_sec);
 
@@ -147,10 +182,10 @@ void loop()
       float temp = my_imu.readTempC();
       temp_characteristic.writeValueLE(temp);
 
-      digitalWrite(LEDB, !digitalRead(LEDB)); // ble heartbeat
+      digitalWrite(LEDB, !digitalRead(LEDB));  // ble heartbeat
       delay(100);
     }
   }
-  digitalWrite(LEDG, !digitalRead(LEDG)); // waiting ble connection
+  digitalWrite(LEDG, !digitalRead(LEDG));  // waiting ble connection
   delay(100);
 }
