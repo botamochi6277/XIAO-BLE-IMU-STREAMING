b568736f01fcf86f6a555d9514b58799dc763d7e
botamochi6277 <botamochi6277@icloud.com>
2023-04-18 16:59:03 +0900

    add celsius unit

--- include/BLEUnit.hpp
+++ include/BLEUnit.hpp
@@ -6,12 +6,14 @@
 #define BLE_GATT_CPF_UNIT_KG 0x2702      // mass(kg)
 #define BLE_GATT_CPF_UNIT_SEC 0x2703     // time(second)
 #define BLE_GATT_CPF_UNIT_AMPERE 0x2704  // current intensity(ampere)
-#define BLE_GATT_CPF_UNIT_KELVIN 0x2705  // tmp(kelvin)
+#define BLE_GATT_CPF_UNIT_KELVIN 0x2705  // temperature(kelvin)
 
 #define BLE_GATT_CPF_UNIT_METER_PER_S 0x2712   // velocity(m/s)
 #define BLE_GATT_CPF_UNIT_METER_PER_SS 0x2713  // acc(m/s^2)
 
 #define BLE_GATT_CPF_UNIT_RAD 0x2720  // angle(radian)
+
+#define BLE_GATT_CPF_UNIT_CELSIUS 0x272F  // Celsius temperature
 
 #define BLE_GATT_CPF_UNIT_RAD_PER_S 0x2743  // angular velocity (rad/s)
 
